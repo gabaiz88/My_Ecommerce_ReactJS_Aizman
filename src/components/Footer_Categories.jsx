0be26ba16b82ad41b5e1782a1f701aa6c65c2101
Gabriel Aizman <gabaiz88@gmail.com>
2023-03-06 23:43:03 -0300

    terminando footer categorias

--- src/components/Footer_Categories.jsx
+++ src/components/Footer_Categories.jsx
@@ -1,15 +1,33 @@
+import { Link } from "react-router-dom";
+
 const Footer_Categories = () => {
   return (
     <div className="footer_categories">
-        <h2>Categorías</h2>
-        <ul>
-            <li><h3>PlayStation 4</h3></li>
-            <li><h3>PlayStation 5</h3></li>
-            <li><h3>Xbox</h3></li>
-            <li><h3>Nintendo</h3></li>
-        </ul>
+      <h2>Categorías</h2>
+      <ul>
+        <Link to={`/category/${"Ps4"}`}>
+          <li>
+            <h3>PlayStation 4</h3>
+          </li>
+        </Link>{" "}
+        <Link to={`/category/${"Ps5"}`}>
+          <li>
+            <h3>PlayStation 5</h3>
+          </li>
+        </Link>
+        <Link to={`/category/${"Xbox"}`}>
+          <li>
+            <h3>Xbox</h3>
+          </li>
+        </Link>{" "}
+        <Link to={`/category/${"Nintendo"}`}>
+          <li>
+            <h3>Nintendo</h3>
+          </li>
+        </Link>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Footer_Categories
+export default Footer_Categories;
