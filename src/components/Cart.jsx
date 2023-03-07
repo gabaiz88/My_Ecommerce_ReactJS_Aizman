d3c5d67211207dba64c345c9de6237e805920c1f
Gabriel Aizman <gabaiz88@gmail.com>
2023-03-07 19:03:37 -0300

    creando contacto

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,32 @@
-import { Flex } from "@chakra-ui/react"
+import { Link } from "react-router-dom";
 import FormCart from "./FormCart"
+import { Button } from "@chakra-ui/react";
 
 const Cart = () => {
-  return (
-    <>
-    <div className="cart_">
-      <p>Aca va el resumen de la compra</p>
-      <FormCart />
-    </div>
-    </>
-  )
+  let cart = "";
+  
+  if (cart === "dasd") {
+    return (
+      <>
+      <div className="cart_">
+        <h2>El carrito esta vacio..</h2>
+        <Link to={"/catalogue"}>
+          <Button colorScheme='red'>Ir al catálogo</Button>
+        </Link>
+      </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+      <div className="cart_">
+        <p>Aca va el resumen de la compra</p>
+        <FormCart />
+      </div>
+      </>
+    )
+  }
+  
 }
 
 export default Cart
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,30 @@
-import Contact_Box from "./Contact_Box"
+import { Textarea, Container } from "@chakra-ui/react";
+import { useState } from "react";
 
 const Contact = () => {
+  let [value, setValue] = useState("");
+
+  let handleInputChange = (e) => {
+    let inputValue = e.target.value;
+    setValue(inputValue);
+    console.log(value);
+  };
+
   return (
     <div>
-        <Contact_Box/>
+      <div className="contact_box">
+        <h2 id="title">Dejanos tu mensaje</h2>
+        <div className="text_area">
+          <Textarea
+            value={value}
+            onChange={handleInputChange}
+            placeholder="Dejanos tu mensaje aca.."
+            size="lg"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
